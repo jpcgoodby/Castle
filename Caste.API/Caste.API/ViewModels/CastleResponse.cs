8f2bd629354731d7450672c423a7e0d8c1a4e859
jpcgoodby <jpcgoodby@yahoo.co.uk>
2021-02-24 22:35:52 +0000

    Complete Service
    Complete Service

---
+++ Caste.API/Caste.API/ViewModels/CastleResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Castle.ViewModels
+{
+    public class CastleResponse
+    {
+        public string Name { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Location { get; set; }
+
+        public string Country { get; set; }
+
+        public int DateBuilt { get; set; }
+
+        public string Url { get; set; }
+    }
+}
