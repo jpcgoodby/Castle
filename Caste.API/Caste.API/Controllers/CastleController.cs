bbf365479056f55ee4c150a009d067bf898cb402
jpcgoodby <jpcgoodby@yahoo.co.uk>
2021-02-25 06:13:36 +0000

    Re add
    Re add

---
+++ Caste.API/Caste.API/Controllers/CastleController.cs
@@ -0,0 +1,59 @@
+﻿using Caste.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Caste.API.Controllers
+{
+    [ApiController]
+    [Route("castle")]
+    public class CastleController : ControllerBase
+    {
+
+        private readonly CastleService _castleService;
+
+        public CastleController(CastleService castleService)
+        {
+            _castleService = castleService;
+        }
+
+        [HttpGet]
+        [Route("data")]
+        [ProducesResponseType(typeof(List<Castle.ViewModels.CastleResponse>), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.Conflict)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [Produces("application/json")]
+        public async Task<ActionResult> Get()
+        {
+            var result =  await _castleService.GetCastles();
+            return Ok(result);
+        }
+
+
+        [HttpPost()]
+        [Route("data")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.Conflict)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [Consumes("application/json")]
+        public async Task<ActionResult> Post([FromBody] Castle.ViewModels.CastleRequest meta)
+        {
+            var result = await _castleService.createCastle(meta);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("file")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.Conflict)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var result = await _castleService.CreateFile(file);
+            return Ok(result);
+        }
+
+    }
+}
---
+++ Caste.API/Caste.API/Startup.cs
@@ -0,0 +1,64 @@
+using Caste.API.Services;
+using Castle.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Caste.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<CastleContext>(options => options.UseInMemoryDatabase(databaseName: "Castles"));
+            services.AddScoped<CastleService, CastleService>();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            var allowedOrigins = Configuration["AllowedOrigins"];
+
+            app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins(
+                allowedOrigins
+            ));
+
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Castle API");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
